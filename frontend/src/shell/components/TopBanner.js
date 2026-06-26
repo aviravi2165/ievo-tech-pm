1c202c1b5380d4b8f40619ab1b55c2ca35edefd8
Jatin Kumawat <jatinkumawat806@gmail.com>
2026-06-26 15:32:53 +0530

    mgm bug

--- frontend/src/shell/components/TopBanner.js
+++ frontend/src/shell/components/TopBanner.js
@@ -51,7 +51,7 @@ export default function TopBanner({ currentUser, activeModule }) {
               <line x1="19" y1="8" x2="19" y2="14"/>
               <line x1="22" y1="11" x2="16" y2="11"/>
             </svg>
-            <span>User Mgmt</span>
+            <span>Manage Users</span>
           </button>
         )}
 
